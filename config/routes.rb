ec511288cdc74d802b2d5ff9e13ca335c817791e
Tenzin Migmar <lhasaboy2010@gmail.com>
2018-12-05 10:34:00 -0500

    Adding all the setting to my backend.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :profiles, except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
+  resources :users, except: %i[new edit]
 
   # Custom routes
   post '/sign-up' => 'users#signup'
